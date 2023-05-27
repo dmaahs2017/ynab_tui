8d5e348c7d1a5983a9f7ae62f60119f77793b50b
Dalton Maahs <dalton.maahs@yourbind.com>
2023-05-27 10:03:35 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use crossterm::{event::*, terminal::*, *};
 use std::io;
 use tui::{backend::*, layout::*, widgets::*, *};
 
+
 pub struct App {
     page_stack: Vec<Box<dyn Page>>,
     restore_stack: Vec<Box<dyn Page>>,
@@ -24,62 +25,74 @@ impl App {
     }
 
     pub fn run(&mut self) -> std::io::Result<()> {
-        enable_raw_mode()?;
-        let mut stdout = io::stdout();
-        execute!(stdout, EnterAlternateScreen, DisableMouseCapture)?;
-        let backend = CrosstermBackend::new(stdout);
-        let mut terminal = Terminal::new(backend)?;
+        let mut terminal = setup_terminal()?;
 
         loop {
             let names: Vec<String> = self.page_stack.iter().map(|p| p.name()).collect();
             let path = names.join(" -> ");
-            if let Some(page) = self.page_stack.last_mut() {
-                terminal.draw(|f| {
-                    let chunks = Layout::default()
-                        .constraints([Constraint::Percentage(10), Constraint::Percentage(90)])
-                        .direction(Direction::Vertical)
-                        .split(f.size());
-                    let page_area = chunks[1];
-                    let path_area = chunks[0];
-                    let path = Paragraph::new(path);
 
-                    f.render_widget(path, path_area);
-                    page.ui(f, page_area);
-                })?;
+            let Some(page) = self.page_stack.last_mut() else { continue };
 
-                let msg = page.update(&mut self.data_gate)?;
-                match msg {
-                    Message::Quit => break,
-                    Message::Back => {
-                        if self.page_stack.len() > 1 {
-                            self.restore_stack.push(self.page_stack.pop().unwrap());
-                        }
+            terminal.draw(|f| {
+                let chunks = Layout::default()
+                    .constraints([Constraint::Percentage(10), Constraint::Percentage(90)])
+                    .direction(Direction::Vertical)
+                    .split(f.size());
+                let page_area = chunks[1];
+                let path_area = chunks[0];
+                let path = Paragraph::new(path);
+
+                f.render_widget(path, path_area);
+                page.ui(f, page_area);
+            })?;
+
+            let msg = page.update(&mut self.data_gate)?;
+
+            match msg {
+                Message::Quit => break,
+                Message::Back => {
+                    if self.page_stack.len() > 1 {
+                        self.restore_stack.push(self.page_stack.pop().unwrap());
                     }
-                    Message::Forward => {
-                        if self.restore_stack.is_empty() {
-                            self.page_stack.push(self.restore_stack.pop().unwrap());
-                        }
-                    }
-                    Message::NewPage(newpage) => {
-                        self.page_stack.push(newpage);
-                        self.restore_stack.clear();
-                    }
-                    Message::Noop => {}
                 }
+                Message::Forward => {
+                    if self.restore_stack.is_empty() {
+                        self.page_stack.push(self.restore_stack.pop().unwrap());
+                    }
+                }
+                Message::NewPage(newpage) => {
+                    self.page_stack.push(newpage);
+                    self.restore_stack.clear();
+                }
+                Message::Noop => {}
             }
         }
 
         // restore terminal
-        disable_raw_mode()?;
-        execute!(
-            terminal.backend_mut(),
-            LeaveAlternateScreen,
-            DisableMouseCapture
-        )?;
-        terminal.show_cursor()?;
+        restore_terminal(terminal)?;
 
         Ok(())
     }
+}
+
+fn setup_terminal() -> io::Result<Terminal<CrosstermBackend<io::Stdout>>> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, DisableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let terminal = Terminal::new(backend)?;
+    Ok(terminal)
+}
+
+fn restore_terminal(mut terminal: Terminal<CrosstermBackend<io::Stdout>>) -> io::Result<()> {
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+    Ok(())
 }
 
 fn main() -> std::io::Result<()> {
